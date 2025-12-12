8165adc24fccdcb66c7b00196f75e49f3b8f09a8
Mahabub Ahmed <mahabub.ahmed.eee@gmail.com>
2025-12-12 15:47:40 +0100

    add arcjet for security middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
 import { time } from 'drizzle-orm/mysql-core';
 import { timestamp } from 'drizzle-orm/gel-core';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+
+app.use(securityMiddleware)
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
---
+++ src/middleware/security.middleware.js
@@ -0,0 +1,55 @@
+import aj from "../config/arcjet.js";
+import logger from '#config/logger.js';
+import { slidingWindow } from "@arcjet/node";
+
+const securityMiddleware = async (req, res, next) => {
+  try {
+    const role = req.user?.role || 'guest';
+
+    let limit;
+    let message
+
+    switch (role) {
+        case 'admin':
+            limit = 20;
+            message = 'Admin request limit exceeded (20 requests per 2 seconds). Slow down!';
+        break;
+        case 'user':
+            limit = 10;
+            message = 'User request limit exceeded (10 requests per 2 seconds). Slow down!';
+        break;
+        case 'guest':
+            limit = 5;
+            message = 'Guest request limit exceeded (5 requests per 2 seconds). Slow down!';
+        break;
+    }
+    const client = aj.withRule(slidingWindow({
+      mode: "LIVE",
+      interval: '1m',
+      max: limit,
+      name: `${role}-rate-limit`
+    }));
+    const decision = await client.protect(req);
+    if (decision.isDenied() && decision.reason.isBot()){
+      logger.warn('Bot request blocked', {IP: req.ip, userAgent: req.get('user-agent'), path: req.path});
+      return res.status(429).json({ error: "Too Many Requests", message: 'Bot activity detected. Access denied.' });
+    }
+
+    if (decision.isDenied() && decision.reason.isShield()){
+      logger.warn('Shield request blocked', {IP: req.ip, userAgent: req.get('user-agent'), path: req.path, method: req.method});
+      return res.status(429).json({ error: "Too Many Requests", message: 'Shield activity detected. Access denied.' });
+    }
+
+    if (decision.isDenied() && decision.reason.isRateLimit()){
+      logger.warn('Rate limit request blocked', {IP: req.ip, userAgent: req.get('user-agent'), path: req.path});
+      return res.status(429).json({ error: "Too Many Requests", message: 'Bot activity detected. Access denied.' });
+    }
+    next();
+
+  } catch (e) {
+    console.error("Security middleware error:", e);
+        res.status(500).json({ error: "Internal Server Error!", message: 'Something went wrong with security middleware' });
+  }
+};
+
+export default securityMiddleware;
